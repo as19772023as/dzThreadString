81fbf52abe345b87b5eac11837872e71291371fb
Andrey Strebkov <as19772023as@gmail.com>
2023-11-23 16:51:56 +0300

    Option real

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,24 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        List<Thread> threadList = new ArrayList<>();
+
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
         long startTs = System.currentTimeMillis(); // start time
 
-        List<Thread> threads = new ArrayList<>();
-        Thread threadN;
         for (String text : texts) {
-
-            threadN = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(threadN);
-            threadN.start();
+            threadList.add(getNewThread(text));
+        }
+        for (Thread thread : threadList) {
+            thread.start();
+        }
+        for (Thread thread : threadList) {
+            thread.join();
         }
 
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-        }
-        
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + " ms");
     }
@@ -53,5 +32,31 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    public static Thread getNewThread(String text) {
+        return new Thread(
+                () -> {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
+                            }
+                        }
+                    }
+                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                });
+
     }
 }
